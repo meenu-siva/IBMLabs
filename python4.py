5d778874d824ca43503d7e43c3f86f646bc8257b
meenu-siva <60912816+meenu-siva@users.noreply.github.com>
2020-02-10 23:02:26 -0800

    Add files via upload

---
+++ python4.py
@@ -0,0 +1,7 @@
+num1 = int(input("enter a number:"))
+num2 = int(input("enter a number:"))
+num3 = int(input("enter a number:"))
+num4 = int(input("enter a number:"))
+
+a = (((num1+num2)*num3)/num4)
+print(a)
