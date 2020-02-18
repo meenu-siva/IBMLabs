3240d9638633a2df755194495a323cab84d415fc
meenu-siva <60912816+meenu-siva@users.noreply.github.com>
2020-02-17 22:53:39 -0800

    Add files via upload

---
+++ conversion.py
@@ -0,0 +1,5 @@
+dec = 222
+print("the decimal value of",dec,"is:")
+print(bin(dec), "in binary")
+print(oct(dec), "in octal")
+print(hex(dec), "in hexadecimal")
---
+++ year.py
@@ -0,0 +1,5 @@
+import calendar
+yy = int(input("enter the year:"))
+mm = int(input("enter the month:"))
+print(calendar.calendar(yy))
+print(calendar.month(yy,mm))
